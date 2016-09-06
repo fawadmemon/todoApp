dfa6b6815575d42d5e4bb34607188b04b8fbbe72
Fawad <memon.dev@gmail.com>
2016-09-06 22:06:47 +0500

    first comit

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   staticFileGlobs: [
+    '/images/**/*',
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
-  handleFetch :false,
+  //handleFetch :false,
   navigateFallback: '/index.html',
+  // navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ],
   runtimeCaching:[{
     urlPattern:/^https:\/\/fonts.(?:googleapis|gstatic).com\/.*/,
     handler:'cacheFirst'
